7aa8303ad487b1de22b44ec69d8eae3da82813fc
zhuravlevmaksim <zhuravlev_mo_18@mf.grsu.by>
2021-01-17 14:18:57 +0300

    'Linked list' model realization, levels are separated into 'InputLayer' and 'HiddenLayer'. Drawing graphs from top to bottom

---
+++ layers.py
@@ -0,0 +1,104 @@
+from typing import Callable, Optional, Any, List
+
+from abc import ABC, abstractmethod
+# import inspect
+import numpy as np
+
+
+
+class Layer(ABC):
+    """
+    An abstract class that implements functionality of the perceptron layer
+
+    Attributes
+    ----------
+    name: str
+        name of the class
+    n_nodes: int
+        number of nodes in layer
+    """
+
+
+    def __init__(self, name, n_nodes) -> None:
+        self.name = name
+        self.n_nodes = n_nodes
+
+
+    @abstractmethod
+    def get_output(self, inputs) -> List[float]:
+        pass
+
+
+
+class HiddenLayer(Layer):
+    """Class for storing and processing hidden layer data
+
+    Methods
+    -------
+
+    Attributes
+    ----------
+    name: str
+        name of the layer
+    n_nodes: int
+        number of the layer nodes
+
+    activation: function
+        activation function of the layer
+    __weights: list[float]
+        the layer weights
+    __bias: float
+        the layer bias
+    """
+
+
+    def __init__(self, name, n_nodes, activation, parent) -> None:
+        super().__init__(name=name, n_nodes=n_nodes)
+        self.activation = activation
+        self.parent = parent
+
+        self.__weights = np.ones((self.n_nodes, self.parent.n_nodes)).tolist()
+        self.__bias = np.zeros(self.n_nodes).tolist()
+
+
+    def get_output(self, inputs) -> List[float]:
+        """
+        That method gets inputs (vector with numbers, calculated on parent
+        layer), computes the layer calculations and return output vector
+        """
+        outputs = []
+
+        # 1. multiplying the input vector by the weight matrix
+        # 2. adding the bias verctor
+        for i in range(self.n_nodes):
+            outputs.append( np.add( np.multiply(inputs, self.__weights[i]),
+                                    self.__bias[i] )
+                          )
+        # 3. applying activation function
+        return (self.activation(outputs)).tolist()
+
+
+
+class InputLayer(Layer):
+    """
+    Input layer of the perceptron
+
+
+    Attributes
+    ----------
+    name: "x"
+        name of the class(always "x")
+    n_nodes: int
+        the layer nodes (number of the perceptron inputs)
+    """
+
+
+    def __init__(self, n_nodes) -> None:
+        super().__init__(name="x", n_nodes=n_nodes)
+
+
+    def get_output(self, inputs):
+        """
+        That method just returns the inputs, because input layer of perceptron
+        has no any calculations"""
+        return inputs
--- model.py
+++ model.py
@@ -2,7 +2,7 @@
 import networkx as nx
 
 from typing import Callable, Optional, List
-from layer import Layer
+from layers import Layer, HiddenLayer, InputLayer
 
 
 
@@ -13,16 +13,18 @@ class MultilayerPerceptron:
 
     Attributes
     ----------
-    __layers: List[Layer]
-        list of layers in newral perceptron
+    __last_layer: HiddenLayer
+        list of layers in perceptron
+    n_inputs: int
+        the number of perceptron inputs
     depth: int
         the number of perceptron layers
     """
 
-    def __init__(self, n_inputs: int) -> None:
+    def __init__(self, n_inputs) -> None:
         # input layer initialization
-        self.__layers = [Layer(name = "x", activation=None, n_nodes=n_inputs,
-                                is_input_layer=True)]
+        self.__last_layer = InputLayer(n_inputs)
+        self.n_inputs = n_inputs
         self.depth = 0
 
 
@@ -36,21 +38,35 @@ class MultilayerPerceptron:
             if perceptron already have a layer with passed name
         """
 
-        if name not in [layer.name for layer in self.__layers]:
-            self.__layers.append( Layer(name=name, activation=activation, n_nodes=n_nodes,
-                                        prev_layer_nodes_number=self.__layers[-1].n_nodes) )
+        layer_names = [layer.name for layer in self.get_layers()]
+        if name not in layer_names:
+            # creating layer if passed name is unique
+            layer = HiddenLayer(name=name, activation=activation,
+                            n_nodes=n_nodes, parent=self.__last_layer)
+            self.__last_layer = layer
+
             self.depth += 1
         else:
             raise NameError(f"Layer with name '{name}' already exists.")
 
 
-    def get_layers(self) -> List:
-        return self.__layers.copy()
+    def get_layers(self) -> List[Layer]:
+        """
+        Method which return perceptron structure (all layers from first to
+        last in list)
+        """
+
+        layers = []
+        layer = self.__last_layer
+        for _ in range(self.depth):
+            layers.append(layer.parent)
+            layer = layer.parent
+        return layers[::-1]
 
 
     def run(self, inputs):
         output = []
-        for layer in self.__layers:
+        for layer in self.get_layers():
             output = layer.get_output(inputs)
             inputs = output
 
@@ -60,8 +76,8 @@ class MultilayerPerceptron:
         """Method for creating graph which shows all connections between layers"""
 
         graph = nx.DiGraph()
-        for curr_layer, next_layer in zip(self.__layers[:-1],
-                                            self.__layers[1:]):
+        for curr_layer, next_layer in zip(self.get_layers()[:-1],
+                                            self.get_layers()[1:]):
             for i in range(curr_layer.n_nodes):
                 for j in range(next_layer.n_nodes):
                     graph.add_edge("$%s_{%i}}$" % (curr_layer.name, i), "$%s_{%i}}$" % (next_layer.name, j))
@@ -72,7 +88,7 @@ class MultilayerPerceptron:
         """Method for creating a graph with abbreviated layer names"""
 
         graph = nx.DiGraph()
-        for curr_layer, next_layer in zip(self.__layers[:-1],
-                                            self.__layers[1:]):
+        for curr_layer, next_layer in zip(self.get_layers()[:-1],
+                                            self.get_layers()[1:]):
             graph.add_edge(f"${curr_layer.name}$", f"${next_layer.name}$")
         return graph
